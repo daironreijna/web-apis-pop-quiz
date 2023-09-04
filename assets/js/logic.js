900a3acabf7cf7829f63136ff51706cc6ae85de7
Dairon Reijna <dairon.reijna@gmail.com>
2023-09-04 20:07:09 +0100

    fixed issue with timer not going to zero, added further code to getQuestion function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,31 +9,10 @@ const timeEL = document.querySelector("#time");
 
 const startButton = document.querySelector("#start");
 
-let timeLeft = 100 // (Needs to be 3 minutes for ten questions)
-// let minutesLeft = 2
-// let secondsLeft = 59
+let timeLeft = 10 // (Needs to be 3 minutes for ten questions)
+let minutesLeft = 2
+let secondsLeft = 59
 
-//TODO! problem with timer, the count down of minutes and seconds stops at 0:1, also the count down may better be 100s and easier to program the deduction of 10s for wrong answers.
-
-function timer(event) {
-    // Prevent default action
-    event.preventDefault();
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-
-        // As long as the `timeLeft` is greater than 1
-        if (timeLeft > 0){
-            // Set the `textContent` of `timerEl` to show the remaining seconds
-            timeEL.textContent = (`${timeLeft}`);
-            // Decrement `timeLeft` by 1
-            timeLeft--;
-            
-        }  else {
-            // Stops execution of action at set interval
-            clearInterval(timerInterval);
-        }
-    },1000);
-};
 
 // function timer(event) {
 //     // Prevent default action
@@ -42,29 +21,50 @@ function timer(event) {
 //     var timerInterval = setInterval(function() {
 
 //         // As long as the `timeLeft` is greater than 1
-//         if (secondsLeft > 0){
+//         if (timeLeft > 0){
 //             // Set the `textContent` of `timerEl` to show the remaining seconds
-//             timeEL.textContent = (`${minutesLeft} : ${secondsLeft}`);
+//             timeEL.textContent = (`${timeLeft}`);
 //             // Decrement `timeLeft` by 1
-//             secondsLeft--;
-            
-//         } else if (minutesLeft > 0 && secondsLeft === 0  ) {
-//             minutesLeft--;
-//             secondsLeft = 59;
-//             // Calls function to create and append highscore
-//            // sendHighscore();
-//         } else if (minutesLeft === 0 && secondsLeft === 1) {
-//             secondsLeft = 0 
-//             timeEL.textContent = (`${minutesLeft} : ${secondsLeft}`);
-            
+//             timeLeft--;
             
 //         }  else {
-//             secondsLeft--;
+//             timeEL.textContent = (`${timeLeft}`)
 //             // Stops execution of action at set interval
 //             clearInterval(timerInterval);
 //         }
 //     },1000);
 // };
+
+function timer(event) {
+    // Prevent default action
+    event.preventDefault();
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+
+        // As long as the `timeLeft` is greater than 1
+        if (secondsLeft > 0){
+            // Set the `textContent` of `timerEl` to show the remaining seconds
+            timeEL.textContent = (`${minutesLeft} : ${secondsLeft}`);
+            // Decrement `timeLeft` by 1
+            secondsLeft--;
+            
+        } else if (minutesLeft > 0 && secondsLeft === 0  ) {
+            timeEL.textContent = (`${minutesLeft} : ${secondsLeft}`);
+            minutesLeft--;
+            secondsLeft = 59;
+            // Calls function to create and append highscore
+           // sendHighscore();
+        } else if (minutesLeft === 0 && secondsLeft === 1) {
+            secondsLeft = 0 
+            timeEL.textContent = (`${minutesLeft} : ${secondsLeft}`);
+                        
+        }  else {
+            timeEL.textContent = (`${minutesLeft} : ${secondsLeft}`);
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+        }
+    },1000);
+};
 
 startButton.addEventListener("click", timer);
 
